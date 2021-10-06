c09e81d78290830a527ca898f2460a78ca627dda
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-10-06 22:53:03 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+/*
+ * Author: Karen Bassem Thabet
+ * File Name: third_mini_project.h
+ * Description: fan controller system header file
+ * Date: 5/10/2021
+ */
+
+#ifndef THIRD_MINI_PROJECT_H_
+#define THIRD_MINI_PROJECT_H_
+
+#include <stdio.h>
+#include "common_macros.h"
+#include "std_types.h"
+#include "avr\io.h"
+#include "gpio.h"
+#include "LCD_driver.h"
+#include "ADC_driver.h"
+#include "LM35_sensor.h"
+#include <util\delay.h>
+
+#endif /* THIRD_MINI_PROJECT_H_ */
