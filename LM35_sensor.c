2cc96e83fd79773c620a74056d05ffee50a0ca68
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-10-13 21:28:54 +0200

    Update LM35_sensor.c

--- LM35_sensor.c
+++ LM35_sensor.c
@@ -1,14 +1,18 @@
-/*
- * Author: Karen Bassem Thabet
- * File Name: LM35_sensor.c
- * Description: Source file for LM35 sensor
- * Module: LM35 sensor
- */
+/******************************************************************************
+ * Module: Temperature LM35 Sensor
+ * File Name: lm35_sensor.c
+ * Description: source file for the LM35 Temperature Sensor driver
+ * Author: Karen Bassem
+ *******************************************************************************/
 
 #include "LM35_sensor.h"
+#include "LCD_driver.h"
+#include "ADC_driver.h"
 
-uint8 LM35_GetTemperature(){
-	uint16 digital_value;
-	digital_value= ADC_readChannel(ADC_CHANNEL_USED);  /* pass channel number used to be read */
-	return ((uint8)(((uint32)(digital_value*SENSOR_MAX_TEMP*VREF))/(SENSOR_MAX_VOLT*ADC_MAX_VALUE)));
+/* Function to calculate the temperature from the ADC digital value */
+uint8 LM35_getTemperature(void)
+{
+	uint16 adc_value = 0;
+	adc_value = ADC_readChannel(ADC_CHANNEL_USED); /* read the channel where the sensor is connected */
+	return (uint8)(((uint32)adc_value*SENSOR_MAX_TEMP*VREF)/(ADC_MAX_VALUE*SENSOR_MAX_VOLT)); /* Calculate the temperature using equation*/
 }
